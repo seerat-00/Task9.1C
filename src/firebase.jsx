15af3d2fa12369f8727a5243e3c84fbfa5e2685b
seerat-00 <iseeratsharmash@gmail.com>
2024-11-08 22:55:05 +0530

    Update firebase.js

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,12 @@ import {getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDt4liB_XYMe--Cj9oJbNiR6QQgHUs-feU",
-    authDomain: "login-web-2c25a.firebaseapp.com",
-    projectId: "login-web-2c25a",
-    storageBucket: "login-web-2c25a.appspot.com",
-    messagingSenderId: "742458172800",
-    appId: "1:742458172800:web:2dde9689e12639b7e74d65"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
 
